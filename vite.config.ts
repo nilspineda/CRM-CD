202bf4cc834d4b856d28134717e599cb2f235f61
nilspineda <nilspineda@gmail.com>
2026-05-06 16:45:10 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
-import react from '@vite-plugin-react'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
